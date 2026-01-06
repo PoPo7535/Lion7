943334daf07297682803040189760d82abf320bd
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-01-06 22:36:24 +0900

    2026_01_06

---
+++ Lion7/Lion7/Day2026_01_06.cs
@@ -0,0 +1,102 @@
+﻿namespace MyApp;
+
+public class Day2026_01_06
+{
+    void Task1()
+    {
+        Console.Write("이름을 입력하세요 : ");
+        string name = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine($"안녕하세요 {name}님");
+    }
+
+    void Task2()
+    {
+        Console.Write("나이를 입력하세요 : ");
+        string age = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine($"내년에는 {int.Parse(age) + 1}살이 되겠군요!");
+    }
+
+    void Task3()
+    {
+        Console.Write("2진수를 입력하세요 : ");
+        string binaryInput = Console.ReadLine() ?? string.Empty;
+        int decimalNumber = Convert.ToInt32(binaryInput,2);
+        Console.WriteLine($"{decimalNumber}");
+            
+        Console.WriteLine($"{Convert.ToString(decimalNumber)}");
+    }
+
+    void Task4()
+    {
+        Console.Write("캐릭터 이름을 입력하세요 : ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"환영합니다, {name}님!");
+
+        Console.Write("시작레벨을 입력하세요 : ");
+        int level = int.Parse(Console.ReadLine());
+        Console.WriteLine($"{name}님의 시작 레벨은 {level}입니다");
+    }
+
+    void Task5()
+    {
+        var name = "Garen";
+        var age = -999;
+        var isDemasia = true;
+        Console.WriteLine($"이름 : {name}, 나이 : {age}, 여부 : {isDemasia}");
+    }
+
+    void Task6()
+    {
+        // input 5 8 4
+            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int a = inputs[0]; // 5
+            int b = inputs[1]; // 8
+            int c = inputs[2]; // 4
+
+            
+            int maxHP = 100;
+            int currentHP = 80;
+            Console.WriteLine($"초기 체력 : {currentHP}/{maxHP}");
+            currentHP -= 25;
+            Console.WriteLine($"데미지 -25 : {currentHP}/{maxHP}");
+            currentHP += 30;
+            Console.WriteLine($"회복 : {currentHP}/{maxHP}");
+            currentHP += 5;
+            Console.WriteLine($"독 데미지 : {currentHP}/{maxHP}\n");
+            
+            int expPerMonster = 150;
+            int monstersKilled = 3;
+            int expForLevelUp = 500;
+            Console.WriteLine($"처치한 몬스터 : {monstersKilled}마리");
+            Console.WriteLine($"획득 경험치 : {expPerMonster * monstersKilled}");
+            Console.WriteLine($"레벨업 까지 필요 : {expForLevelUp - expPerMonster* monstersKilled}\n");
+
+            int totalGold = 1234;
+            int partyMembers = 5;
+            Console.WriteLine($"총 골드 : {totalGold}");
+            Console.WriteLine($"파티원 : {partyMembers}");
+            Console.WriteLine($"1인당 골드 : {totalGold / partyMembers}");
+            Console.WriteLine($"남은 골드 : {totalGold % partyMembers}\n");
+            
+            int playerLevel = 35;
+            int requiredLevel = 30;
+            bool hasKey = true;
+            currentHP = 60;
+            maxHP = 100;
+            Console.WriteLine($"=== 던전 입장 조건 ===");
+            var levelKey = requiredLevel <= playerLevel;
+            Console.WriteLine($"레벨 조건 (30 이상) : {levelKey}");
+            Console.WriteLine($"열쇠 보유 : {hasKey}");
+            var hpKey = 50 <= (maxHP / currentHP * 100);
+            Console.WriteLine($"체력 조건 (50% 이상) : {hpKey}");
+            Console.WriteLine($"입장 가능 : {levelKey && hpKey && hasKey}\n");
+
+            float originalPrice = 5000;
+            Console.WriteLine($"원가 : {originalPrice} 골드");
+            originalPrice *= 0.2f;
+            Console.WriteLine($"VIP 할인 (20%) : {originalPrice} 골드");
+            originalPrice -= 500;
+            Console.WriteLine($"쿠폰 할인 (-500) : {originalPrice} 골드");
+            Console.WriteLine($"최종 가격 : {originalPrice} 골드");
+    }
+}
--- Lion7/Lion7/Program.cs
+++ Lion7/Lion7/Program.cs
@@ -1,13 +1,85 @@
 ﻿using System;
+using System.Drawing;
 
 namespace MyApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var arr = new int[26];
+            Array.Fill(arr, -1);
+            var input = Console.ReadLine();
+            for (int i = 0; i < input.Length; ++i)
+            {
+                var index = input[i] - 'a';
+                if (-1 == arr[index])
+                    arr[index] = i;
+            }
 
+            foreach (var value in arr)
+                Console.Write($"{value} ");
         }
 
+        void ASD()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var arr = new int[N, N];
+            var blueCount = 0;
+            var whiteCount = 0;
+
+            for (int y = 0; y < N; ++y)
+            {
+                var n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                for (int x = 0; x < N; ++x)
+                {
+                    arr[y, x] = n[x];
+                }
+            }
+
+            var queue = new Queue<Rectangle>();
+            queue.Enqueue(new Rectangle(0, 0, N, N));
+            while (0 != queue.Count)
+            {
+                var rect = queue.Dequeue();
+                var startColor = IsWhite(rect.Y, rect.X);
+                bool breaking = false;
+                for (int y = 0; y < rect.Height; ++y)
+                {
+                    for (int x = 0; x < rect.Width; ++x)
+                    {
+                        if (startColor != IsWhite(y + rect.Y, x + rect.X))
+                        {
+                            breaking = true;
+                            break;
+                        }
+                    }
+
+                    if (breaking)
+                        break;
+                }
+
+                if (false == breaking)
+                {
+                    if (startColor)
+                        ++whiteCount;
+                    else
+                        ++blueCount;
+                }
+                else if (0 != rect.Width / 2)
+                {
+                    var size = rect.Width / 2;
+                    queue.Enqueue(new Rectangle(rect.X, rect.Y, size, size));
+                    queue.Enqueue(new Rectangle(rect.X + size, rect.Y, size, size));
+                    queue.Enqueue(new Rectangle(rect.X, rect.Y + size, size, size));
+                    queue.Enqueue(new Rectangle(rect.X + size, rect.Y + size, size, size));
+                }
+            }
+
+            Console.WriteLine(whiteCount);
+            Console.WriteLine(blueCount);
+            bool IsWhite(int y, int x) => arr[y, x] == 0;
+
+        }
     }
 }
